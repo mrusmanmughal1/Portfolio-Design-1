4dad7319199dcea7f8400fd79bafa95d1e611532
Muhammad Usman <mrusmanmughal1@gmail.com>
2023-08-27 10:01:25 +0500

    Portfolio Design 1 first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+function  Home (){
+    return (<div>
+    
+<div className="main">
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+    <div className="box"></div>
+</div>    
+    
+        </div>)
+}
+export default Home;
